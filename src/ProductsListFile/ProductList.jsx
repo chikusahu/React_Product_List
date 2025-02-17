0d28cbfdb86ddc57aff67a35ea0f8ff118e99857
chikusahu <chandan050201@gmail.com>
2025-02-17 18:00:44 +0530

    Added product filter using state

--- src/ProductsListFile/ProductList.jsx
+++ src/ProductsListFile/ProductList.jsx
@@ -1,16 +1,29 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import ProductCard from "./ProductCard";
 
-function ProductList(props){
+function ProductList(props) {
     const { products } = props;
+    const [search, setSearch] = useState("");
+    const filteredItem = products.filter((product) =>
+        product.name.toLowerCase().includes(search.toLowerCase()));
 
-    return(
-        <div style={{display:"flex", flexWrap:"wrap"}}>
-            {products.map((detail)=>(
-               <ProductCard data={detail} key={detail.id}/>
-            ))}
+    return (
+        <div>
+            <input type="text"
+                placeholder="Search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)} />
+
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+
+                {filteredItem.map((detail) => (
+                    <ProductCard data={detail} key={detail.id} />
+
+                ))}
+            </div>
+
         </div>
     )
 }
